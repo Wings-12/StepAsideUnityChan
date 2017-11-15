cc99157a881daeba2b54aa2880dfcf598ffdd6c6
Tsubasa.Oumi <ohmiwing@gmail.com>
2017-11-16 07:00:06 +0900

    Unitychanが横移動・ジャンプ可能
    Unitychanを矢印キー（左右）を押すと左右に移動し、
    spaceキーを押すとジャンプするようにした。

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -13,6 +13,9 @@ public class UnityChanController : MonoBehaviour {
     //左右に移動するための力（追加）
     private float turnForce = 500.0f;
 
+    //ジャンプするための力（追加）
+    private float upForce = 500.0f;
+
     //左右の移動できる範囲（追加）
     private float movableRange = 3.4f;
 
@@ -36,6 +39,30 @@ void Update () {
         this.myRigidbody.AddForce(this.transform.forward * this.forwardForce);
 
         //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる（追加）
+        if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x)
+        {
+            //左に移動（追加）
+            this.myRigidbody.AddForce(-this.turnForce, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange)
+        {
+            //右に移動（追加）
+            this.myRigidbody.AddForce(this.turnForce, 0, 0);
+        }
 
+        //Jumpステートの場合はJumpにfalseをセットする（追加）
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+
+        //ジャンプしていない時にスペースが押されたらジャンプする（追加）
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        {
+            //ジャンプアニメを再生（追加）
+            this.myAnimator.SetBool("Jump", true);
+            //Unityちゃんに上方向の力を加える（追加）
+            this.myRigidbody.AddForce(this.transform.up * this.upForce);
+        }
     }
 }
