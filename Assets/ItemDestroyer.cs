10463a486cc3f1abdfdc5da88feb452dbedd8252
Tsubasa.Oumi <ohmiwing@gmail.com>
2017-11-20 22:37:38 +0900

    ユニティちゃんが通り過ぎて画面外に出たアイテムを直ちに破棄してくださいの課題完了
    ・Lesson6で作成したStepAsideUnityChanをもとに作成してください
    ・ユニティちゃんが通り過ぎて画面外に出たアイテムを直ちに破棄してください
    ・アイテムの破棄にはDestroy関数を使いましょう
    の３つの条件もクリア！
    
    無数のアイテムを生成するオブジェクトはそれぞれを指定することが難しいので、
    別なスクリプトを書いて、ステージ開始後にコインなどのオブジェクトを生成する素である「プロジェクトビュー」のプレハブ
    にスクリプトをアタッチしてやって欲しいことを頼むことを学んだ。

--- Assets/ItemDestroyer.cs
+++ Assets/ItemDestroyer.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class ItemDestroyer : MonoBehaviour
 {
-    //carPrefabを入れる
-    public GameObject carPrefab;
-    //coinPrefabを入れる
-    public GameObject coinPrefab;
-    //cornPrefabを入れる
-    public GameObject conePrefab;
 
     //課題１内容：コイン・コーン・車（以下アイテムと呼ぶ）を生成するオブジェクトをUnityちゃんがを
     //通り過ぎて画面外に行ったら破棄する（課題１）
@@ -33,7 +27,7 @@ public class ItemDestroyer : MonoBehaviour
         //アイテムが画面外に出た場合（課題１）コメント：ゲームを再生したときにアイテムが画面外で破棄されているかわからない
         //if (this.transform.position.z < this.visiblePosZ)
         //上記コメントのため、次はUnityちゃんを通り過ぎたときにアイテムをが破棄されるようにする
-        if (this.transform.position.z < this.unityChan.GetComponent<Transform>().position.z)
+        if (this.transform.position.z < this.unityChan.GetComponent<Transform>().position.z -6.5f)
         {
             //アイテムを破棄する（課題１）
             //if (this.gameObject.tag == "ItemGeneratorTag")
